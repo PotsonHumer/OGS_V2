6354a5e1883517a174256514f76ab35082da7fbc
PotsonHumer <potsonhumer@gmail.com>
2016-04-17 17:16:41 +0800

    修正前台部落格程式

--- class/blog/frontend.php
+++ class/blog/frontend.php
@@ -15,19 +15,19 @@
 			self::$temp = CORE::$temp_main;
 			
 			CORE::common_resource();
-			CORE::res_init('news/data_list','css');
+			CORE::res_init('blog/data_list','css');
 			
-			self::$temp["MAIN"] = 'ogs-news-tpl.html';
+			self::$temp["MAIN"] = 'ogs-blog-tpl.html';
 
 			if(!empty($cate)){
-				self::$cate = SEO::origin('news_cate',$cate);
-				self::$temp["MAIN"] = 'ogs-news-tpl.html';
+				self::$cate = SEO::origin('blog_cate',$cate);
+				self::$temp["MAIN"] = 'ogs-blog-tpl.html';
 				$func++;
 			}
 			
 			if(!empty($args)){
-				self::$id = SEO::origin('news',$args);
-				self::$temp["MAIN"] = 'ogs-news-detail-tpl.html';
+				self::$id = SEO::origin('blog',$args);
+				self::$temp["MAIN"] = 'ogs-blog-detail-tpl.html';
 				$func++;
 			}
 
@@ -48,17 +48,17 @@
 			CORE::res_init('fix','css');
 
 			if(!empty(self::$cate)){
-				$rsnum = CRUD::dataFetch('news',array('parent' => self::$cate,'status' => '1','langtag' => CORE::$langtag),false,array('sort' => CORE::$cfg["sort"]));
+				$rsnum = CRUD::dataFetch('blog',array('parent' => self::$cate,'status' => '1','langtag' => CORE::$langtag),false,array('sort' => CORE::$cfg["sort"]));
 			}else{
-				$rsnum = CRUD::dataFetch('news',array('status' => '1','langtag' => CORE::$langtag),false,array('sort' => CORE::$cfg["sort"]));
+				$rsnum = CRUD::dataFetch('blog',array('status' => '1','langtag' => CORE::$langtag),false,array('sort' => CORE::$cfg["sort"]));
 			}
 
 			if(!empty($rsnum)){
-				#VIEW::newBlock("TAG_NEWS_BLOCK");
+				#VIEW::newBlock("TAG_BLOG_BLOCK");
 				$dataRow = CRUD::$data;
 
 				foreach($dataRow as $key => $row){
-					VIEW::newBlock("TAG_NEWS_LIST");
+					VIEW::newBlock("TAG_BLOG_LIST");
 					foreach($row as $field => $var){
 						switch($field){
 							case "showdate":
@@ -79,7 +79,7 @@
 						}
 					}
 					
-					IMAGES::load('news',$row["id"]);
+					IMAGES::load('blog',$row["id"]);
 					list($images) = IMAGES::$data;
 					VIEW::assign(array(
 						"VALUE_LINK" => self::dataLink($row["parent"],$row),
@@ -90,19 +90,19 @@
 				}
 
 				# SEO
-				$cate_rsnum = CRUD::dataFetch('news_cate',array('id' => self::$cate));
+				$cate_rsnum = CRUD::dataFetch('blog_cate',array('id' => self::$cate));
 				if(!empty($cate_rsnum)){
 					list($cate_row) = CRUD::$data;
 					SEO::load($cate_row["seo_id"]);
 					if(empty(SEO::$data["h1"])) SEO::$data["h1"] = $cate_row["subject"];
 				}else{
-					SEO::load('news');
-					if(empty(SEO::$data["h1"])) SEO::$data["h1"] = CORE::$lang["news"];
+					SEO::load('blog');
+					if(empty(SEO::$data["h1"])) SEO::$data["h1"] = CORE::$lang["blog"];
 				}
 
 				SEO::output();
 
-				CRUMBS::fetch('news',$cate_row);
+				CRUMBS::fetch('blog',$cate_row);
 			}else{
 				VIEW::newBlock("TAG_NONE");
 			}
@@ -110,15 +110,15 @@
 
 		# 選單
 		private static function nav(){
-			VIEW::assignGlobal("NAV_CATE_TITLE",'NEWS');
-			$rsnum = CRUD::dataFetch('news_cate',array('status' => '1','langtag' => CORE::$langtag),false,array('sort' => CORE::$cfg["sort"]));
+			VIEW::assignGlobal("NAV_CATE_TITLE",'BLOG');
+			$rsnum = CRUD::dataFetch('blog_cate',array('status' => '1','langtag' => CORE::$langtag),false,array('sort' => CORE::$cfg["sort"]));
 			if(!empty($rsnum)){
 				$dataRow = CRUD::$data;
 				foreach($dataRow as $key => $row){
 					VIEW::newBlock("TAG_NAV_LIST");
 					VIEW::assign(array(
 						"VALUE_NAV_SUBJECT" => $row["subject"],
-						"VALUE_NAV_LINK" => CORE::$root.'news/'.SEO::link($row).'/',
+						"VALUE_NAV_LINK" => CORE::$root.'blog/'.SEO::link($row).'/',
 						"VALUE_NAV_CURRENT" => (self::$id == $row["id"])?'current':'',
 					));
 				}
@@ -127,27 +127,27 @@
 
 		# 顯示內容
 		private static function detail(){
-			$rsnum = CRUD::dataFetch('news',array('id' => self::$id));
+			$rsnum = CRUD::dataFetch('blog',array('id' => self::$id));
 			if(!empty($rsnum)){
 				list($row) = CRUD::$data;
 				foreach($row as $field => $var){
 					VIEW::assignGlobal("VALUE_".strtoupper($field),$var);
 				}
 
-				VIEW::assignGlobal("VALUE_BACK_LINK",CORE::$root.'news/');
+				VIEW::assignGlobal("VALUE_BACK_LINK",CORE::$root.'blog/');
 
 				SEO::load($row["seo_id"]);
 				if(empty(SEO::$data["h1"])) SEO::$data["h1"] = $row["subject"];
 				SEO::output();
 
-				CRUMBS::fetch('news',$row);
+				CRUMBS::fetch('blog',$row);
 				self::hot(self::$id);
 			}
 		}
 
 		# 熱門文章列表
 		private static function hot($nowID){
-			$rsnum = CRUD::dataFetch('news',array('status' => '1','hot' => '1','id' => '!'.$nowID),false,array('rand()' => ''));
+			$rsnum = CRUD::dataFetch('blog',array('status' => '1','hot' => '1','id' => '!'.$nowID),false,array('rand()' => ''));
 			if(!empty($rsnum)){
 				VIEW::newBlock("TAG_HOT_BLOCK");
 				$dataRow = CRUD::$data;
@@ -164,7 +164,7 @@
 						VIEW::assign("VALUE_".strtoupper($field),$var);
 					}
 
-					IMAGES::load('news',$row["id"]);
+					IMAGES::load('blog',$row["id"]);
 					list($images) = IMAGES::$data;
 					VIEW::assign(array(
 						"VALUE_IMAGE" => $images["path"],
