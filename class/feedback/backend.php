9bed2d0d72b3e129c5c66384f85cf1fc4e0ba8d8
PotsonHumer <potsonhumer@gmail.com>
2016-04-18 11:42:56 +0800

    增加反饋功能

---
+++ class/feedback/backend.php
@@ -0,0 +1,203 @@
+<?php
+
+	# 後台反饋管理
+
+	class FEEDBACK_BACKEND extends OGSADMIN{
+		function __construct(){
+			
+			list($func,$id) = CORE::$args;
+			$nav_class = 'FEEDBACK';
+
+			switch($func){
+				case "add":
+					self::$temp["MAIN"] = 'ogs-admin-intro-insert-tpl.html';
+					self::$temp["SEO"] = self::$temp_option["SEO"];
+					CORE::res_init('tab','box');
+					self::add();
+				break;
+				case "insert":
+					self::$temp["MAIN"] = self::$temp_option["MSG"];
+					self::insert();
+				break;
+				case "detail":
+					self::$temp["MAIN"] = 'ogs-admin-intro-modify-tpl.html';
+					self::$temp["SEO"] = self::$temp_option["SEO"];
+					CORE::res_init('tab','box');
+					self::detail($id);
+				break;
+				case "modify":
+					self::$temp["MAIN"] = self::$temp_option["MSG"];
+					self::modify();
+				break;
+				case "del":
+					self::$temp["MAIN"] = self::$temp_option["MSG"];
+					self::delete($id);
+				break;
+				case "multi":
+					self::$temp["MAIN"] = self::$temp_option["MSG"];
+					parent::multi('intro',CORE::$manage.'intro/');
+				break;
+				case "multiChange":
+					self::$temp["MAIN"] = self::$temp_option["MSG"];
+					$idArray = parent::multiChange('intro');
+
+					if(is_array($idArray)){
+						foreach($idArray as $id){
+							$_POST['id'] = $id;
+							self::modify();
+						}
+					}
+				break;
+				default:
+					self::$temp["MAIN"] = 'ogs-admin-intro-list-tpl.html';
+					self::row();
+				break;
+			}
+
+			self::nav_current($nav_class,$nav_func);
+		}
+
+		# 反饋列表
+		private static function row(){
+			$rsnum = CRUD::dataFetch('intro',array('langtag' => CORE::$langtag),false,array('sort' => CORE::$cfg["sort"]),false,true);
+			if(!empty($rsnum)){
+				VIEW::newBlock("TAG_FEEDBACK_BLOCK");
+
+				$data = CRUD::$data;
+				foreach($data as $key => $row){
+					VIEW::newBlock("TAG_FEEDBACK_LIST");
+					foreach($row as $field => $var){
+						switch($field){
+							case "status":
+								$status = ($var)?self::$lang["status_on"]:self::$lang["status_off"];
+								if(empty($var)) VIEW::assign("CLASS_STATUS_RED",'red');
+								VIEW::assign("VALUE_".strtoupper($field),$status);
+							break;
+							default:
+								VIEW::assign("VALUE_".strtoupper($field),$var);
+							break;
+						}
+					}
+
+					VIEW::assign('VALUE_NUMBER',PAGE::$start + (++$i));
+				}
+			}else{
+				VIEW::newBlock("TAG_NONE");
+			}
+		}
+
+		# 新增反饋
+		private static function add(){
+			$rsnum = CRUD::dataFetch('intro',array("langtag" => CORE::$langtag));
+			CRUD::args_output(true,true);
+			VIEW::assignGlobal("VALUE_SORT",++$rsnum);
+		}
+
+		# 執行新增
+		private static function insert(){
+			CHECK::is_must($_POST["callback"],$_POST["subject"],$_POST["content"]);
+
+			if(CHECK::is_pass()){
+				CRUD::dataInsert('intro',$_POST,true,true);
+				if(!empty(DB::$error)){
+					CRUD::args_output();
+					$msg = DB::$error;
+					$path = CORE::$manage.'intro/add/';
+				}else{
+					$msg = self::$lang["modify_done"];
+					$path = CORE::$manage.'intro/';
+				}
+			}else{
+				CRUD::args_output();
+				$msg = CHECK::$alert;
+				$path = CORE::$manage.'intro/add/';
+			}
+
+			CORE::msg($msg,$path);
+		}
+
+		# 反饋詳細
+		private static function detail($id){
+			$rsnum = CRUD::dataFetch('intro',array('id' => $id));
+			if(!empty($rsnum)){
+				list($row) = CRUD::$data;
+				foreach($row as $field => $var){
+					switch($field){
+						case "status":
+							VIEW::assignGlobal("VALUE_".strtoupper($field)."_CK".$var,'selected');
+						break;
+						default:
+							VIEW::assignGlobal("VALUE_".strtoupper($field),$var);
+						break;
+					}
+				}
+
+				$last_page = SESS::get("PAGE");
+				if(!empty($last_page)){
+					VIEW::assignGlobal("VALUE_BACK_LINK",CORE::$manage."intro/page-{$last_page}/");
+				}else{
+					VIEW::assignGlobal("VALUE_BACK_LINK",CORE::$manage."intro/");
+				}
+
+				parent::$langID = $row['lang_id'];
+
+				SEO::load($row["seo_id"]);
+				SEO::output();
+			}else{
+				self::$temp["MAIN"] = self::$temp_option["MSG"];
+				CORE::msg(self::$lang["no_args"],CORE::$manage.'intro/');
+			}
+		}
+
+		# 修改反饋
+		private static function modify(){
+			CHECK::is_must($_POST["callback"],$_POST["id"],$_POST["subject"],$_POST["content"]);
+			$check = CHECK::is_pass();
+			$rsnum = CRUD::dataFetch('intro',array('id' => $_POST["id"]));
+
+			if($check && !empty($rsnum)){
+				CRUD::dataUpdate('intro',$_POST,true);
+				if(!empty(DB::$error)){
+					$msg = DB::$error;
+					$path = CORE::$manage.'intro/';
+				}else{
+					$msg = self::$lang["modify_done"];
+					#$path = CORE::$manage."intro/detail/{$_POST['id']}/";
+					$path = $_SERVER['HTTP_REFERER'];
+				}
+			}else{
+				if(empty($rsnum)){
+					$msg = self::$lang["no_data"];
+					$path = CORE::$manage.'intro/';
+				}
+
+				if(!$check){
+					$msg = CHECK::$alert;
+					$path = CORE::$manage.'intro/';
+				}
+			}
+
+			CORE::msg($msg,$path);
+		}
+
+		# 刪除反饋
+		private static function delete($id){
+			$rs = CRUD::dataDel('intro',array('id' => $id));
+			if(!empty(DB::$error)){
+				$msg = DB::$error;
+				$path = CORE::$manage.'intro/';
+			}
+
+			if(!$rs){
+				$msg = self::$lang["del_error"];
+				$path = CORE::$manage.'intro/';
+			}else{
+				$msg = self::$lang["del_done"];
+				$path = CORE::$manage.'intro/';
+			}
+
+			CORE::msg($msg,$path);
+		}
+	}
+
+?>
---
+++ class/feedback/frontend.php
@@ -0,0 +1,86 @@
+<?php
+
+	# 前台反饋功能
+
+	class FEEDBACK_FRONTEND extends FEEDBACK{
+
+		private static 
+			$temp,
+			$m_id, # 會員 id
+			$id; # 資料 id
+
+		function __construct(){
+
+			list($args) = CORE::$args;
+			self::$m_id = SESS::get('m_id');
+			self::$temp = CORE::$temp_main;
+
+			#CRUMBS::fetch('feedback');
+
+			switch($args){
+				case "list": # 顯示反饋
+					self::$temp["MAIN"] = 'ogs-feedback-list-tpl.html';
+					self::row();
+				break;
+				case "add": # 執行增加反饋
+					self::$temp["MAIN"] = CORE::$temp_option["MSG"];
+					self::add();
+				break;
+				default:
+					self::$temp["MAIN"] = 'ogs-feedback-tpl.html';
+				break;
+			}
+
+			CORE::common_resource();
+
+			new VIEW(CORE::$temp_option["HULL"],self::$temp,false,false);
+		}
+
+
+		# 反饋顯示
+		private static function row(){
+			$rsnum = CRUD::dataFetch('feedback',array('status' => '1'),false,array('createdate' => 'desc'));
+			if(!empty($rsnum)){
+				VIEW::newBlock('TAG_FEEDBACK_BLOCK');
+				$dataRow = CRUD::$data;
+				foreach($dataRow as $row){
+					VIEW::newBlock('TAG_FEEDBACK_LIST');
+					foreach($row as $field => $var){
+						VIEW::assign('VALUE_'.strtoupper($field),$var);
+					}
+
+					# 顯示評分星星數
+					if(!empty($row['score'])){
+						VIEW::newBlock('TAG_SCORE_BLOCK');
+						VIEW::assign('VALUE_SCORE',$row['score']);
+						$score = 0;
+
+						while(++$score <= $row['score']){
+							VIEW::newBlock('TAG_SCORE_STAR');
+						}
+					}
+				}
+			}
+		}
+
+		# 增加反饋
+		private static function add(){
+			CHECK::is_must($_POST["callback"],$_POST["name"],$_POST["content"]);
+			CHECK::is_email($_POST["email"]);
+
+			if(CHECK::is_pass()){
+				CRUD::dataInsert('feedback',$_POST);
+				if(!empty(DB::$error)){
+					$msg = 'Error! '.DB::$error;
+				}else{
+					$msg = CORE::$lang['submit_done'];
+				}
+			}else{
+				$msg = CHECK::$alert;
+			}
+
+			CORE::msg($msg,CORE::$root.'feedback/');
+		}
+	}
+
+?>
