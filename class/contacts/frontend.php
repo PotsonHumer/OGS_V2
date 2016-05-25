8ce386ed2eb82be4d03ff44e7cc5b5651a8dc3f7
PotsonHumer <potsonhumer@gmail.com>
2016-05-25 09:48:47 +0800

    補上 contact 驗證 & 主題顯示功能

--- class/contacts/frontend.php
+++ class/contacts/frontend.php
@@ -32,6 +32,8 @@
 
 		# 顯示聯絡我們
 		private static function form($m_id=false){
+			self::subjectFetch();
+			
 			if(!empty($m_id)){
 				CRUD::dataFetch('member',array('id' => $m_id));
 				list($row) = CRUD::$data;
@@ -56,12 +58,31 @@
 			CRUMBS::fetch('contact');
 		}
 
+		# 取得主題
+		private static function subjectFetch(){
+			$rsnum = CRUD::dataFetch('contact_subject',false,false,array('sort' => CORE::$cfg['sort']));
+			if(!empty($rsnum)){
+				$dataRow = CRUD::$data;
+				VIEW::newBlock("TAG_SUBJECT_NAME");
+
+				foreach($dataRow as $row){
+					VIEW::newBlock('TAG_SUBJECT_LIST');
+					VIEW::assign('VALUE_SUBJECT',$row['subject']);
+				}
+			}else{
+				VIEW::newBlock('TAG_SUBJECT_LIST');
+				VIEW::assign('VALUE_SUBJECT','無類型');
+			}
+		}
+
 		# 紀錄聯絡我們
 		private static function add($m_id=false){
+			$recaptcha = self::recaptcha();
+
 			CHECK::is_must($_POST["name"],$_POST["tel"],$_POST["content"]);
 			CHECK::is_email($_POST["email"]);
 
-			if(CHECK::is_pass()){
+			if($recaptcha && CHECK::is_pass()){
 				$args = array(
 					'm_id' => (!empty($m_id))?$m_id:"null",
 					'content' => htmlspecialchars($_POST["content"],ENT_NOQUOTES),
@@ -92,10 +113,40 @@
 				}
 			}else{
 				$msg = CHECK::$alert;
+				if(empty($recaptcha)) $msg = '請勾選驗證';
 				CRUD::args_output();
 			}
 
 			CORE::msg($msg,CORE::$root.'contact/');
+		}
+
+		# 驗證
+		private static function recaptcha(){
+			$toURL = "https://www.google.com/recaptcha/api/siteverify";
+			$post = array(
+				"secret" => "6LdAdx0TAAAAAO0gu8Vz131Xfos5R6E5bxTxdkzD",
+				"response" => $_POST["g-recaptcha-response"],
+			);
+
+			$ch = curl_init();
+			$options = array(
+				CURLOPT_URL => $toURL,
+				CURLOPT_HEADER => 0,
+				CURLOPT_VERBOSE => 0,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_USERAGENT => "Mozilla/4.0 (compatible;)",
+				CURLOPT_POST => true,
+				CURLOPT_POSTFIELDS => http_build_query($post),
+			);
+
+			curl_setopt_array($ch, $options);
+			// CURLOPT_RETURNTRANSFER=true 會傳回網頁回應,
+			// false 時只回傳成功與否
+			$result = curl_exec($ch); 
+			curl_close($ch);
+			
+			$rsArray = json_decode($result,true);
+			return $rsArray["success"];
 		}
 	}
 
