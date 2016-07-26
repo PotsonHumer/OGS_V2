53d432b83be0b62fb97406b9968eeac89581700d
PotsonHumer <potsonhumer@gmail.com>
2016-07-26 12:40:47 +0800

    改進 system 輸出處理

--- class/system/index.php
+++ class/system/index.php
@@ -19,37 +19,46 @@
 			CORE::call_function(self::$endClass,$function,$args);
 		}
 
+		# 資料輸出處裡
+		private static function argsHandle($prefix='SYSTEM',array $row){
+			foreach($row as $field => $var){
+				switch($field){
+					case "address":
+						$output[$prefix.'_'.strtoupper($field).'_TARGET'] = (!empty($var))?'_blank':'_self';
+						$output[$prefix.'_MAP'] = (!empty($var))?'https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode&q='.$var:'#';
+					break;
+					case "tel":
+					case "cell":
+						$output[$prefix.'_'.strtoupper($field).'_LINK'] = preg_replace('/[^\d]/','',$var);
+					break;
+					case "ga":
+						$var = self::gaLoad($var);
+					break;
+					case "facebook":
+					case "gplus":
+					case "twitter":
+					case "instagram":
+					case "linkedin":
+						$output[$prefix.'_'.strtoupper($field).'_TARGET'] = (!empty($var))?'_blank':'_self';
+						$var = (empty($var))?'#':'';
+					break;
+				}
+
+				$output[$prefix.'_'.strtoupper($field)] = $var;
+			}
+
+			return (is_array($output))?$output:false;
+		}
+
 		# 讀取系統設定
 		public static function setting(){
 			$rsnum = CRUD::dataFetch('system',array('id' => '1'));
 			if(!empty($rsnum)){
-				self::$setting = CRUD::$data[0];
-				foreach(self::$setting as $field => $var){
-					switch($field){
-						case "address":
-							if(!empty($var)) $output['SYSTEM_MAP'] = 'https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode&q='.$var;
-						break;
-						case "ga":
-							$var = self::gaLoad($var);
-						break;
-						case "email":
-							if(empty($var)){ # 如果未設定系統 E-mail，設定初始 E-mail
-								$var = 'potsonhumer@gmail.com';
-								self::$setting[$field] = $var;
-							}
-						break;
-						case "facebook":
-						case "gplus":
-						case "twitter":
-						case "instagram":
-						case "linkedin":
-							$output['SYSTEM_'.strtoupper($field).'_TARGET'] = (!empty($var))?'_blank':'_self';
-							$var = (empty($var))?'#':'';
-						break;
-					}
+				list($row) = CRUD::$data;
+				$output = self::argsHandle('SYSTEM',$row);
 
-					$output['SYSTEM'.strtoupper($field)] = $var;
-				}
+				if(empty($row['email'])) $row['email'] = 'potsonhumer@gmail.com';
+				self::$setting = $row;
 
 				VIEW::assignGlobal($output);
 			}
@@ -73,9 +82,8 @@
 				$dataRow = CRUD::$data;
 				foreach($dataRow as $row){
 					VIEW::newBlock("{$tag}_SYSTEM_CUSTOM");
-					foreach($row as $field => $var){
-						VIEW::assign('VALUE_'.strtoupper($field),$var);
-					}
+					$output = self::argsHandle('VALUE',$row);
+					VIEW::assign($output);
 				}
 			}
 		}		
